23b0592b80cd6795b86d886c4b110b57c460f996
GUSTAVO PADRAO DI IORIO AGUIAR <gpiorio@gmail.com>
2017-02-01 16:33:18 -0200

    Protótipo de tela
    Tela para gamification da TI - CEMIG.
    Usando o PHP, CSS.

--- UX, UI - modelo/index.php
+++ UX, UI - modelo/index.php
@@ -3,35 +3,45 @@
 <head>
 	<title>TI Gamification</title>
         <link rel="stylesheet" type="text/css" href="_css/estilo_gamification.css">
+        <meta charset="utf-8">
 </head>
 <body>
-	<div id="pagina_pontuacao">
-		<header>
-			
-		</header>
-		<nav>
-			<a href="index.php">Rank Geral</a>
-			<a href="">Suas medalhas e seus feitos</a>
-		</nav>
-	</div>
-	<div>
-            <table>
-                <tr>
-                    <th>#</th>
-                    <th>Participante</th>
-                    <th>Pontos</th>
-                    <th>Nível</th>
-                </tr>
+    <div id="menu">
+	<header>
+            <h1>Gamification</h1>		
+	<nav>
+            <span id="rank_geral" class="botao"><a href="index.php">Rank Geral</a></span>
+            <span id="myAvatar" class="botao"><a href="">Seu Avatar e feitos</a></span>
+	</nav>
+        </header>
+    </div>
+    <div id="pagina_pontuacao">
+        <table id="rank">
+            <tr>
+                <th>#</th>
+                <th>Participante</th>
+                <th>Pontos</th>
+                <th>Nível</th>
+            </tr>
                 
-		<?php
-			/*Pega o último id de usário ou faz a contagem de registro e coloca o total na variavel*/
-			$user = 10;
-			/*Gera as linhas para nome e pontuação de forma dinamica, ambos os campos não tem permissão de edição*/
-			for ($i = 0; $i < $user ; $i++) { 
-				print "<tr><td><b>$i<b/></td><td>Nome do jogador com link para um perfil</td><td>Pontuação total</td><td>Rank</td></tr>";
-			}
-		?>
-            </table>
-	</div>
+            <?php
+                /*Pega o último id de usário ou faz a contagem de registro e coloca o total na variavel*/
+                $user = 25;
+                /*Gera as linhas para nome e pontuação de forma dinamica, ambos os campos não tem permissão de edição*/
+		for ($i = 1; $i <= $user ; $i++) { 
+		print "<tr><td><b>$i<b/></td><td>Nome do jogador com link para um perfil</td><td>Pontuação total</td><td>Rank</td></tr>";
+		}
+            ?>
+        </table>
+    </div>
+    <footer>
+        <table id="rodape">
+            <tr>
+                <td class="lateral"><img src="_imagens/logo_TI.png" id="logo_ti" class="rodape"></td>
+                <td class="central"><span class="rodape">Superintendência de Tecnologia da informação e Telecomunicação</span></td>
+                <td class="lateral"><img src="_imagens/CEMIG.png" id="logo_cemig" class="rodape"></td>
+            </tr>
+        </table>
+    </footer>
 </body>
 </html>
